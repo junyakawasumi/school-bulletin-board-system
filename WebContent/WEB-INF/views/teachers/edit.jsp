a577e92abc52494953a889377e405fc4adc8a6c1
junyakawasumi <saru69ing@icloud.com>
2020-09-01 22:19:21 +0900

    Add Teachers Edit

---
+++ WebContent/WEB-INF/views/teachers/edit.jsp
@@ -0,0 +1,32 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<c:import url="/WEB-INF/views/layout/app.jsp">
+    <c:param name="content">
+        <c:choose>
+            <c:when test="${teacher != null}"> <%-- リクエストスコープに保存されたteacherがnullではなかった場合の処理 --%>
+                <h2>ID : ${teacher.id} の教職員情報 編集ページ</h2>
+                <p>（パスワードは変更する場合のみ入力してください）</p>
+                <form method="POST" action="<c:url value='/teachers/update' />">
+                    <c:import url="_form.jsp" />
+                </form>
+
+                <p><a href="#" onclick="confirmDestroy();">この教職員情報を削除する</a></p>
+                <form method="POST" action="<c:url value='/teachers/destroy' />">
+                    <input type="hidden" name="_token" value="${_token}" /> <%-- セッションID --%>>
+                </form>
+                <script> <%-- JavaScript --%>
+                    function confirmDestroy() {
+                        if(confirm("本当に削除してよろしいですか？")) {
+                            document.forms[1].submit();
+                        }
+                    }
+                </script>
+            </c:when>
+            <c:otherwise>
+                <h2>お探しのデータは見つかりませんでした。</h2>
+            </c:otherwise>
+        </c:choose>
+
+        <p><a href="<c:url value='/teachers/index' />">一覧に戻る</a></p>
+    </c:param>
+</c:import>
---
+++ src/controllers/teachers/TeachersEditServlet.java
@@ -0,0 +1,52 @@
+package controllers.teachers;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Teacher;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class TeachersEditServlet
+ */
+@WebServlet("/teachers/edit")
+public class TeachersEditServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TeachersEditServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //データベースにアクセス
+        EntityManager em = DBUtil.createEntityManager();
+
+        //findメソッドを用いて該当IDのデータ一件をTeacher型のインスタンスに格納
+        Teacher t = em.find(Teacher.class, Integer.parseInt(request.getParameter("id")));
+
+        em.close();
+
+        request.setAttribute("teacher", t); //取得したデータ一件(Teacher型)をリクエストスコープに保存
+        request.setAttribute("_token", request.getSession().getId()); //セッションIDをリクエストスコープに保存
+        request.getSession().setAttribute("teacher_id", t.getId()); //教職員IDをセッションスコープに保存
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/teachers/edit.jsp");
+        rd.forward(request, response);
+
+    }
+
+}
